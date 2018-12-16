8f113aba46c8c9c0c20571a5e64c27d2454db145
jeybone <jeysonp@gmail.com>
2018-12-15 22:14:59 -0600

    started movie function

--- liri.js
+++ liri.js
@@ -1,3 +1,19 @@
+// Read and set environment variables
 require("dotenv").config();
 
-var spotify = new Spotify(keys.spotify);
+var fs = require("fs");
+var axios = require("axios");
+var nodeArgs = process.argv;
+
+// Movie this fuction
+function movieThis(movieSearch) {
+  var movieQueryUrl = "http://www.omdbapi.com/?t=" + movieSearch + "&y=&plot=short&apikey=trilogy";
+axios.get(movieQueryUrl).then(
+  function(response) {
+    console.log("The movie's rating is: " + response.data.imdbRating);
+  }
+);
+
+}
+
+// var search = process.env.slice(3).join(" ")
